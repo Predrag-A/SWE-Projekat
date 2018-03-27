152ef98b8033f5cd80464f1f0ef5f798e73b21a5
Predrag Antanasijevic <antanas686@gmail.com>
2018-03-27 13:39:32 +0200

    Added CourtsTableSeeder to DBSeeder file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(CitiesTableSeeder::class);
         $this->call(SportsTableSeeder::class);
+        $this->call(CourtsTableSeeder::class);
     }
 }
