73ecce6762d4c54b8092fcafcd7b68d2630b1aa0
Predrag Antanasijevic <antanas686@gmail.com>
2018-06-16 00:21:37 +0200

    User List Update

--- resources/views/pages/users/index.blade.php
+++ resources/views/pages/users/index.blade.php
@@ -25,15 +25,19 @@
     <li class="collection-item avatar col s12 l6">
 
       <!-- SLIKA -->
-      <img class="circle" src="{{route('index')}}/storage/avatars/{{$user->user_img}}"> 
+      <img class="circle" src="{{route('index')}}/storage/avatars/{{$user->user_img}}" style="margin-top:25px;"> 
 
-      <!-- PODACI -->
-      <a href="{{route('korisnici')}}/{{$user->id}}" class="blue-text text-darken-2 title"><b>{{$user->first_name}} {{$user->last_name}}</b></a>
-      <p>
-        <i class="material-icons tiny">home</i>
-        <i>{{$user->city->name}}</i>
-        <like-rating :positive_ratings="{{$user->likeCount()}}" :negative_ratings="{{$user->dislikeCount()}}" :user_id="{{$user->id}}" :readonly="true"></like-rating>        
-      </p>
+      <div style="padding-top:10px;">
+        <!-- PODACI -->
+        <a href="{{route('korisnici')}}/{{$user->id}}" class="blue-text text-darken-2 title"><b>{{$user->first_name}} {{$user->last_name}}</b></a>
+        <p>
+          <i class="material-icons tiny">home</i>
+          <i>{{$user->city->name}}</i>
+          <div style="padding-left:13px">
+            <like-rating :positive_ratings="{{$user->likeCount()}}" :negative_ratings="{{$user->dislikeCount()}}" :user_id="{{$user->id}}" :readonly="true"></like-rating>       
+          </div> 
+        </p>
+      </div>
 
       <!-- DUGME -->          
       <span class="secondary-content">
